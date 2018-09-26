c6188c0f911bf52e749356581e1b115c38c8b314
hyperion70 <bmaximov71@gmail.com>
2018-09-26 13:47:29 +0300

    Add extract-files.sh.

---
+++ extract-files.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+## usage: extract-files.sh $1 $2
+## $1 and $2 are optional
+## if $1 = unzip the files will be extracted from zip file (if $1 = anything else 'adb pull' will be used
+## $2 specifies the zip file to extract from (default = ../../../${DEVICE}_update.zip)
+
+VENDOR=Santin
+DEVICE=N1
+BASE=../../../vendor/$VENDOR/$DEVICE
+
+rm -rf $BASE/*
+
+if [ -z "$2" ]; then
+    ZIPFILE=../../../${DEVICE}.zip
+else
+    ZIPFILE=$2
+fi
+
+if [ "$1" = "unzip" -a ! -e $ZIPFILE ]; then
+    echo $ZIPFILE does not exist.
+else
+    for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$`; do
+        DIR=`dirname $FILE`
+	if [ ! -d $BASE/$DIR ]; then
+            mkdir -p $BASE/$DIR
+	fi
+	if [ "$1" = "unzip" ]; then
+            unzip -j -o $ZIPFILE system/$FILE -d $BASE/$DIR
+	else
+            adb pull system/$FILE $BASE/$FILE
+	fi
+    done
+fi
+. setup-makefiles.sh
