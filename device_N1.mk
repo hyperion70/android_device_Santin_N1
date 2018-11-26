a0203f92463650b2f555477464ae8e6de25cb482
hyperion70 <bmaximov71@gmail.com>
2018-11-26 09:28:04 +0300

    Enable NFC

--- device_N1.mk
+++ device_N1.mk
@@ -183,8 +183,18 @@ PRODUCT_PACKAGES += \
 PRODUCT_SYSTEM_SERVER_JARS += com.cyanogenmod.keyhandler
 
 # NFC
+PRODUCT_PACKAGES += \
+    com.android.nfc_extras \
+    NfcNci \
+    Tag
+    
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/libnfc-nxp_RF.conf:system/vendor/libnfc-nxp_RF.conf
+    $(LOCAL_PATH)/configs/libnfc-nxp_RF.conf:system/vendor/libnfc-nxp_RF.conf \
+    $(LOCAL_PATH)/configs/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
+    $(LOCAL_PATH)/configs/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml
 
 # Charger
 PRODUCT_PACKAGES += \
